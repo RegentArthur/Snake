ee7acfb76acd3964a53439641299866a2b0e102f
Caedy Lee <157775115+CaedyLee@users.noreply.github.com>
2024-08-19 22:50:29 -0400

    Update GameWindow.java

--- src/Main/GameWindow.java
+++ src/Main/GameWindow.java
@@ -6,7 +6,8 @@ import java.awt.*;
 public class GameWindow {
     public JFrame frame;
     private GamePanel gamePanel;
-    public JPanel topBar;
+    //public JPanel topBar;
+    public TopBar topBar;
 
     public GameWindow(GamePanel gamePanel){
         frame = new JFrame("Snake Game");
@@ -15,16 +16,23 @@ public class GameWindow {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.gamePanel = gamePanel;
 
+        topBar = new TopBar();
+
         /*
         I am so pissed that i spent 1.5 hours on this
         The first way I did it, was essentially just create another class that extends JPanel for the top bar itself,
         importing all the set field methods. But even though I replicated my rendering for GamePanel, added all the components
         to the containers, somehow, just somehow, the paintComponent() method refused to re-run
          */
+
+        //TODO: maybe isolate this panel? (make it a panel inside the panel topbar -> only repaint this panel, don't repaint topbar)
+        /*
         topBar = new JPanel(new GridBagLayout()){
+
             public void paintComponent(Graphics g){
                 super.paintComponent(g);
 
+                setUpField();
                 String text = "Score: ";
                 Font font = new Font("Serif", Font.ITALIC, 20);
                 FontMetrics metrics = g.getFontMetrics(font);
@@ -38,8 +46,10 @@ public class GameWindow {
             }
         };
 
+         */
+
         frame.add(gamePanel);
-        frame.add(topBar, BorderLayout.NORTH);
+        //frame.add(topBar, BorderLayout.NORTH);
         frame.pack();
         gamePanel.requestFocus(); //this line has to come after .pack()
 
@@ -55,7 +65,39 @@ public class GameWindow {
 
     }
 
-    public void setUpField(){
+    class TopBar extends JPanel{
+
+        public JPanel topBar;
+
+        public TopBar(){
+            topBar = new JPanel(new GridBagLayout());
+            setUpField(topBar);
+            frame.add(topBar,BorderLayout.NORTH);
+        }
+
+        //CANNOT: paintComponent runs immendiately once you add this jpanel to the frame
+        //ok... but why is there no score??
+        //wait no, what i said in the first line doesnt make sense then, cuz if paintComponent runs, then renderScore must also run
+        //ahhh, so paintComponent does NOT run...why???
+        public void paintComponent(Graphics g){
+            super.paintComponent(g);
+            System.out.println("noiyiorev");
+
+            String text = "Score: ";
+            Font font = new Font("Serif", Font.ITALIC, 20);
+            FontMetrics metrics = g.getFontMetrics(font);
+            g.setFont(font);
+
+            int x = getWidth() - 100;
+            int y = (getHeight() - metrics.getHeight())/2 + metrics.getAscent();
+
+            renderScore(g,text,x,y);
+
+        }
+    }
+
+
+    public void setUpField(JPanel topBar){
         //Create the top horizontal bar for the stats and menu
         topBar.setBackground(new Color (27,34,44));
         topBar.setPreferredSize(new Dimension(450, GamePanel.TILES_LENGTH*2)); // I don't think the width value matters
@@ -69,7 +111,6 @@ public class GameWindow {
         gbc.fill = GridBagConstraints.VERTICAL; // centers the text vertically
         gbc.insets = new Insets(0, 40, 0, 0);
 
-        //ToDo: implement the display to show the score
         JLabel scoreLabel = new JLabel("Cutie :)");
         topBar.add(scoreLabel, gbc);
         scoreLabel.setForeground(Color.white);
@@ -78,6 +119,7 @@ public class GameWindow {
     }
 
     public void renderScore(Graphics g, String text, int x, int y){
+        System.out.println("rhuofheroig");
         g.setColor(Color.PINK);
         g.drawString(text + GamePanel.snake.getScore(),x,y);
     }
